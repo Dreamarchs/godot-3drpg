eab9f80e269a15268af3aa1460a6cd3eea45d34b
Deven West <devenw5@gmail.com>
2025-11-29 22:35:07 -0500

    Inventory Start

---
+++ UserInterface/item_icon.gd
@@ -0,0 +1,11 @@
+extends TextureButton
+class_name ItemIcon
+
+signal interact(item)
+
+@onready var stat_label = $MarginContainer/StatLabel
+@onready var item_label = $MarginContainer/ItemLabel
+
+func _on_item_label_gui_input(event: InputEvent) -> void:
+	if event.is_action("click"):
+		interact.emit(self)
